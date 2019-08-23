966312f26e4cf2e8f1a1cf61fb1e52aaeb36f7de
jsfanatik <jsfanatik@gmail.com>
2019-08-23 19:10:19 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import store from './store'
 Vue.config.productionTip = false
 
 firebase.initializeApp({
-  databaseURL: "https://spa-auth-4f312.firebaseio.com",
-  projectId: "spa-auth-4f312",
-  storageBucket: "spa-auth-4f312.appspot.com"
+  databaseURL: "",
+  projectId: "",
+  storageBucket: ""
 });
 
 export const db = firebase.firestore();
